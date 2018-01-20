b489752b5c0874c6fbe21602312a5c51fdc298bc
ThinhLP <thinhlp208@gmail.com>
2018-01-21 02:57:10 +0700

    [Issue #7] Complete master page and js config

---
+++ Frontend/Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+    'use strict';
+    var jsSources = [
+        'js/components/*.js',
+    ];
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+
+            jsBuild: {
+                options: {
+                    banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> (c) 2018 ThinhLP */\n'
+                },
+                files: {
+                    'dist/js/<%= pkg.name %>.min.js': jsSources
+                }
+            },
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
---
+++ Frontend/js/components/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+function Test() {
+    console.log("hihi");
+}
