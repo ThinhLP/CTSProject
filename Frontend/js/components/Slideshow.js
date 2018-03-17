027d0b21e0fc78dce3c9a3904c2c35e9ce51aaaa
ThinhLP <thinhlp208@gmail.com>
2018-03-17 19:11:36 +0700

    Implement slide show javascript

---
+++ Frontend/js/components/Slideshow.js
@@ -0,0 +1,63 @@
+"use strict";
+
+function Slideshow(configs) {
+    var slideShowContainerSlt = configs.slideShowContainer;
+    var slideSelector = slideShowContainerSlt + ' ' + configs.slideSelector;
+    var nextSlideBtn = slideShowContainerSlt + ' ' + configs.nextSlideBtn;
+    var prevSlideBtn = slideShowContainerSlt + ' ' + configs.prevSlideBtn;
+    var slideProgressSlt = slideShowContainerSlt + ' ' + configs.slideProgress
+    var currentSlideSlt = '.current-slide';
+    var totalSlideSlt = '.total-slides';
+    var totalSlides = 0;
+    
+    var slideIndex = !configs.startSlide ? 1 : configs.startSlide;
+    
+    var changeSlide = function(n) {
+        showSlide(slideIndex += n);
+    };
+    
+    var showSlide = function(index) {
+        var $slides = $(slideSelector);
+        if (index > $slides.length) {
+            slideIndex = 1
+        }
+        if (index < 1) {
+            slideIndex = x.length
+        }
+        
+        $slides.hide();
+        $slides.eq(slideIndex - 1).show();
+        setSlideProgress(slideIndex, totalSlides);
+    };
+    
+    var setSlideProgress = function(currentSlide) {
+        $(slideProgressSlt + ' ' + currentSlideSlt).text(currentSlide);
+        $(slideProgressSlt + ' ' + totalSlideSlt).text(totalSlides);
+    };
+    
+    var init = function() {
+        totalSlides = $(slideSelector).length;
+        
+        setSlideProgress(slideIndex, totalSlides);
+        showSlide(slideIndex);
+    };
+    
+    var addEventHandlers = function() {
+        $(nextSlideBtn).on('click', function() {
+            changeSlide(1);
+        });
+        
+        $(prevSlideBtn).on('click', function() {
+            changeSlide(-1);
+        });
+        
+        $(slideSelector).on('click', function() {
+            changeSlide(1);
+        });
+    };
+    
+    init();
+    addEventHandlers();
+    
+    return {};
+}
